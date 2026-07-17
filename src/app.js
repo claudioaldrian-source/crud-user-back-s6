319174ee07b6d15da2fbc948221a4de71cb54daf
Claudio Aldrian <claudioaldrian@gmail.com>
2026-07-17 09:39:02 -0300

    Agrega varios middlewares

--- src/app.js
+++ src/app.js
@@ -1,23 +1,19 @@
-import express from 'express'
-import './config/env.js'
-/* import connectDB from './config/db.js' */
-import  connectDB  from './config/db.js'
-import userRoutes from './routes/user.routes.js'
-import authRoutes from './routes/user.routes.js';
+import express from "express";
+import "./config/env.js";
+import connectDB from "./config/db.js";
+import corsConfig from "./config/cors.js";
+import userRoutes from "./routes/user.routes.js";
+import authRoutes from "./routes/auth.routes.js";
+import { rateLimiter } from "./middlewares/rateLimit.middleware.js";
 
-const app = express()
+const app = express();
 app.set("trust proxy", 1);
-app.use
-
-app.use(express.json())
-
-connectDB()
-
-app.use(userRoutes)
+app.use(corsConfig);
+app.use(express.json());
+app.use(rateLimiter);
+connectDB();
+app.use(userRoutes);
 app.use("/auth", authRoutes);
-
 app.listen(process.env.PORT, () => {
-
-  console.log(`🚀 Servidor corriendo en puerto ${process.env.PORT}`)
-
-})
+  console.log(`🚀 Servidor corriendo en puerto ${process.env.PORT}`);
+});
